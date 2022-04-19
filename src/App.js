03cc3287fbfa94435b16c4ed57e585b5b13d1538
ErlJocson <jocsonerl@gmail.com>
2022-04-20 00:13:47 +0800

    finished button

--- src/App.js
+++ src/App.js
@@ -21,13 +21,15 @@ function App() {
     <div className="main-container">
       {randomFact && dateOfFact ? (
         <>
-          {" "}
           <h1>Random cat fact</h1>
           <div className="paragraph">{randomFact}</div>
           <div className="date-container">
             <p>Created at: {dateOfFact[0].split("T")[0]}</p>
             <p>Updated at: {dateOfFact[1].split("T")[0]}</p>
           </div>
+          <button className="btn" onClick={generateRandomFact}>
+            Another cat fact!
+          </button>
         </>
       ) : (
         ""
